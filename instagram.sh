ddd77da233d1da77dc5096f9c17fd94733f38fda
Gilmarfilho2003 <89646761+Gilmarfilho2003@users.noreply.github.com>
2021-09-13 08:21:24 -0300

    Update instagram.sh

--- instagram.sh
+++ instagram.sh
@@ -1,1 +1,36 @@
+# bin / bash
+#script Gilmar Filho
+#O verdadeiro criador dos arquivos principais não somos nós, então os créditos
+#Vá para o autor real
+#colours
+##############3
+echo
+
+ _           _                                  
+(_)         | |                                 
+ _ _ __  ___| |_ __ _  __ _ _ __ __ _ _ __ ___  
+| | '_ \/ __| __/ _` |/ _` | '__/ _` | '_ ` _ \ 
+| | | | \__ \ || (_| | (_| | | | (_| | | | | | |
+|_|_| |_|___/\__\__,_|\__, |_|  \__,_|_| |_| |_|
+                       __/ |                    
+                      |___/                     
+
+printf "\n"
+printf "                \e[101m\e[1;77m  >>  Script By Gilmar Filho << \e[0m\n"
+printf "\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Gilmar Filho \e[100m\e[1;77m << \e[0m\n"
+printf "\n"
+}
+echo 
+menu1(){
+printf "\e[1;92m[\e[0m\e[1;77m1\e[0m\e[1;92m]\e[0m\e[1;93m Embreve\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m2\e[0m\e[1;92m]\e[0m\e[1;93m Ataque Manual\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m3\e[0m\e[1;92m]\e[0m\e[1;93m EMBREVE\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m4\e[0m\e[1;92m]\e[0m\e[1;93m EMBREVE\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m5\e[0m\e[1;92m]\e[0m\e[1;93m Se inscrever\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m6\e[0m\e[1;92m]\e[0m\e[1;93m EMBREVE now\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m7\e[0m\e[1;92m]\e[0m\e[1;93m Help\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m EMBREVE\e[0m\n"
+printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Saída\e[0m\n"
+read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
 
